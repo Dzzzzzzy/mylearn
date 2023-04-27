92b5898f7dfb62edbdb5a99ad5279c8a33dce901
dongzhenyang1 <dongzhenyang1@jd.com>
2023-04-27 14:29:23 +0800

    push

--- datastructure/Test.java
+++ datastructure/Test.java
@@ -5,58 +5,33 @@
 package datastructure;
 
 public class Test{
-    public static String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int numAIndex = a.length() - 1;
-        int numBIndex = b.length() - 1;
-        int temp = 0;
-        int carry = 0;
-        while (numAIndex >= 0 && numBIndex >= 0) {
-            temp =  Integer.parseInt(String.valueOf(a.charAt(numAIndex))) + Integer.parseInt(String.valueOf(b.charAt(numBIndex))) + carry;
-            carry = 0;
-            if (temp > 1) {
-                temp %= 2;
-                carry = 1;
-            }
-            sb.append(temp);
-            numAIndex--;
-            numBIndex--;
+
+    public static int mySqrt(int x) {
+        // 背景知识，连续个奇数的和为平方数
+        // 1+3+5=9=3^2
+        int count = 0;
+        int sum = 0;
+        int oddNum = 1;
+        while(x >= 0) {
+            x -= oddNum;
+            oddNum += 2;
+            count++;
         }
-        while (numAIndex >= 0) {
-            if (carry == 1) {
-                temp = carry + Integer.parseInt(String.valueOf(a.charAt(numAIndex--)));
-                if (temp == 2) {
-                    temp = 0;
-                    carry = 1;
-                } else {
-                    carry = 0;
-                }
-                sb.append(temp);
-                continue;
-            }
-            sb.append(a.charAt(numAIndex--));
-        }
-        while (numBIndex >= 0) {
-            if (carry == 1) {
-                temp = carry + Integer.parseInt(String.valueOf(b.charAt(numBIndex--)));
-                if (temp == 2) {
-                    temp = 0;
-                    carry = 1;
-                } else {
-                    carry = 0;
-                }
-                sb.append(temp);
-                continue;
-            }
-            sb.append(b.charAt(numBIndex--));
-        }
-        if (carry == 1) {
-            sb.append(carry);
-        }
-        return sb.reverse().toString();
+        return count - 1;
     }
+    
+    
 
     public static void main(String[] args) {
-        System.out.println(addBinary("1001", "11"));
+        System.out.println(mySqrt(2147395600));
+        String s = null;
+        if (1 == 2) {
+            s = "safasf";
+        }
+        if (s == null) {
+            System.out.println(123);
+        } else {
+            System.out.println(345);
+        }
     }
 }
